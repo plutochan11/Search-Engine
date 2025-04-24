4c640543f27d7c61faab7ad783e67ca51a13e7f6
Pluto <109087502+plutochan11@users.noreply.github.com>
2025-04-24 12:05:19 +0800

    Update

--- search-engine/src/main/java/hk/ust/csit5930/CrawlerApplication.java
+++ search-engine/src/main/java/hk/ust/csit5930/CrawlerApplication.java
@@ -3,7 +3,10 @@ package hk.ust.csit5930;
 public class CrawlerApplication {
     public static void main(String[] args) {
         Spider spider = new Spider();
+        long startTime = System.currentTimeMillis();
         spider.crawl();
+        long endTime = System.currentTimeMillis();
         spider.checkResults();
+        System.out.printf("Running time: %d s\n", (endTime - startTime)/1000);
     }
 }
